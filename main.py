01567862a7ca7f73a64cd08073c47ae2b7557641
peterkaloyannis <peter.kaloyannis@outlook.com>
2019-11-02 12:54:56 -0400

    forgot to add main.py!

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+This File will be executed to combine the galaxy simulation with the star simulation in interesting ways.
+PK, AB, GP, AG 2019
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from galaxy import *
+from starsim import *
+from header import *

c3036a67cf760de26aba4ed962f099153f0bd608
peterkaloyannis <peter.kaloyannis@outlook.com>
2019-11-02 12:51:32 -0400

    added the files to be worked on

---
+++ galaxy.py
@@ -0,0 +1,7 @@
+'''
+This file will simulate galaxies based on different mass star distributions, optimally over time scales longer than a star lifetime. It is to be plugged into main.py.
+PK, GP, AG, AB 2019
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from header import *
---
+++ header.py
@@ -0,0 +1,3 @@
+'''
+This file will contain a bunch of default settings and universal constants used by the code but not available from numpy or scipy
+'''

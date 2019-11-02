3f44dca07d8941fc73882c534415818855c6baa5
peterkaloyannis <49298336+peterkaloyannis@users.noreply.github.com>
2019-11-02 16:34:43 -0400

    Update header.py

--- header.py
+++ header.py
@@ -1,3 +1,10 @@
 '''
-This file will contain a bunch of default settings and universal constants used by the code but not available from numpy or scipy
+This file will contain a bunch of default settings and universal constants used by the code but not available from numpy or scipy to be used in all code
 '''
+
+r_range = 1 #maximum radius
+r_step = 0.001 #radial step
+dt = 1 #time step
+
+rho_0 = 1e-21 #kg/m^3
+r_s = 25000 #ly
